6384e77fc9564c9f41d4e11687d9a18294c1f0c9
kennybeeutomo <kennybee132@gmail.com>
2025-02-16 16:47:01 +0700

    alacritty: updated opacity changer script

--- .scripts/ala-opc.sh
+++ .scripts/ala-opc.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/zsh
 # Script to change alacritty opacity
 
-alacrittyConfigPath="$HOME/.config/alacritty/alacritty.toml"
-newOpacity=$(cat $HOME/.config/alacritty/alacritty.toml | tomlq '.window.opacity')
+configDir="$HOME/.config/alacritty"
+configFile="$configDir/alacritty.toml"
+opacityFile="$configDir/alacritty-opacity.toml"
+newOpacity=$(cat "$opacityFile" | tomlq '.window.opacity')
 
 default=0.8
 step=${2:-0.1}
@@ -20,4 +22,5 @@ newOpacity=$(printf "%.1f" "$(($newOpacity))")
 [[ $newOpacity -gt 1 ]] && newOpacity=1
 [[ $newOpacity -lt 0 ]] && newOpacity=0
 
-tomlq -i -t ".window.opacity = $newOpacity" $alacrittyConfigPath || exit 1
+tomlq -i -t ".window.opacity = $newOpacity" "$opacityFile" || exit 1
+touch "$configFile"
