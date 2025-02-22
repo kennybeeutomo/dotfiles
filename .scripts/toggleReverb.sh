23c00929dc967540470d4fe194791fcc614e4461
kennybeeutomo <kennybee132@gmail.com>
2025-02-22 15:09:16 +0700

    toggleReverb: refactor + firefox bind + multiple output handler

--- .scripts/toggleReverb.sh
+++ .scripts/toggleReverb.sh
@@ -2,7 +2,7 @@
 # Script to toggle reverb by switching pipewire links between reverb sink and headphones sink
 
 err() {
-	hyprctl notify 2 2000 0 "$1"
+	hyprctl notify 3 2000 0 "$1"
 	exit 1
 }
 
@@ -16,11 +16,15 @@ null_fl='default_null_sink:playback_FL'
 null_fr='default_null_sink:playback_FR'
 headphones_fl='Headphones:playback_FL'
 headphones_fr='Headphones:playback_FR'
-mpd_fl='mpd.PipeWire Sound Server:output_FL'
-mpd_fr='mpd.PipeWire Sound Server:output_FR'
 
-output_fl="${1:-$mpd_fl}"
-output_fr="${2:-$mpd_fr}"
+mpd='mpd.PipeWire Sound Server'
+
+output_fl="${1:-$mpd}:output_FL"
+output_fr="${1:-$mpd}:output_FR"
+
+if [ $(echo $(pw-filter-id.sh "port.alias" "\"$output_fl\"" | wc -l)) -gt 1 ]; then
+	err 'Multiple outputs'
+fi
 
 pw-link -d "$output_fl" "$null_fl"; pw-link -d "$output_fr" "$null_fr" || {
 	(pw-link "$output_fl" "$null_fl") &
