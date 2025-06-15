22c05b3fe721db278ef1d4c34619f2abb68ecbb4
kennybeeutomo <kennybee132@gmail.com>
2025-06-15 16:54:36 +0700

    screenshot: added monitor selection

--- .scripts/screenshot.sh
+++ .scripts/screenshot.sh
@@ -3,20 +3,28 @@
 
 exitCode=0
 
+screenshotSelection() {
+	wayfreeze &
+	sleep 0.1
+	if [ -z "$1" ]; then
+		grim -g "$(slurp $slurpOpts)"
+	else
+		grim -g "$(slurp $slurpOpts)" "$1"
+	fi
+	exitCode=$?
+	killall wayfreeze
+}
+
 case $1 in
 	fullscreen )
 		grim $path
 		;;
+	monitor )
+		slurpOpts='-o'
+		screenshotSelection "$2"
+		;;
 	selection )
-		wayfreeze &
-		sleep 0.1
-		if [ -z "$2" ]; then
-			grim -g "$(slurp)"
-		else
-			grim -g "$(slurp)" "$2"
-		fi
-		exitCode=$?
-		killall wayfreeze
+		screenshotSelection "$2"
 		;;
 	* )
 		exitCode=1
