27a9efcda36716b47069b607c01431819a14b68b
kennybeeutomo <kennybee132@gmail.com>
2025-07-07 17:56:59 +0700

    added dunst config

--- .scripts/prop.sh
+++ .scripts/prop.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-# Output window class and title through hyprctl notify
+# Output window class and title through dunstify
 
-string=$(printf "Xwayland: %s;Class: %s;Title: %s\n" $(hyprprop | jq -r '.xwayland, .class, .title'))
-IFS=';' read -ra substrings <<< "$string"
-
-for substring in "${substrings[@]}"; do
-    hyprctl -q notify -1 4000 0 "$substring"
-done
+dunstify -i 0 "Window Properties" "$(printf "Xwayland: %s\nClass: %s\nTitle: %s\n" $(hyprprop | jq -r '.xwayland, .class, .title'))"
