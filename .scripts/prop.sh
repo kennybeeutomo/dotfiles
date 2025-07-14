1f1b0b4cbbd55f18209cf088ff89b45e7fe44e21
kennybeeutomo <kennybee132@gmail.com>
2025-07-14 22:09:42 +0700

    prop.sh: fix delimiter problem

--- .scripts/prop.sh
+++ .scripts/prop.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # Output window class and title through dunstify
 
+IFS=$'\n'
 dunstify -i 0 "Window Properties" "$(printf "Xwayland: %s\nClass: %s\nTitle: %s\n" $(hyprprop | jq -r '.xwayland, .class, .title'))"
