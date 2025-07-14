d85100690d5a8c19cf4f71c4446aa2fe494ce08c
kennybeeutomo <kennybee132@gmail.com>
2025-07-14 22:10:13 +0700

    added cpulimiter script

---
+++ .scripts/cpulimiter.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# script to change cpu max performance in percent as in /sys/devices/system/cpu/intel_pstate/max_perf_pct
+
+source ~/.scripts/utils
+
+currPerf=$(cat /sys/devices/system/cpu/intel_pstate/max_perf_pct)
+perfs=$(echo {2..10}0)
+
+perf=$(echo "$perfs" | vmenu -select "$currPerf" -sep ' ' -p 'CPU Max Performance (%)')
+
+[ -z "$perf" ] && exit 1
+
+echo $perf | pkexec tee /sys/devices/system/cpu/intel_pstate/max_perf_pct
